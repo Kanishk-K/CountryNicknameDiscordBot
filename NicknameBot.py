466519356767f56e11277b94c40ec313083121c3
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-10-15 20:45:47 -0500

    Finalized Bot, ready for production.

--- NicknameBot.py
+++ NicknameBot.py
@@ -1,13 +1,25 @@
 import discord
 from discord.ext import commands, tasks
 import os
+import shutil
 import asyncio
 import json
 import re
+
+
 #Setup these enviorment names with the variable name being the name in the quotes
 BOT_ID = os.environ.get('BOT_ID')
+#API_KEY = os.environ.get('API_KEY')
+GUILD_IDS = [715020961291698248]
+BASE_ROLE = 725785246867390517
+DEFAULT_NICKNAME_CHANNEL = "nickname_test"
+#URL = "https://api.jsonbin.io/b/5f47f33a514ec5112d0f825d"
+#headers = {'Content-Type': 'application/json','secret-key':API_KEY}
+
 
 client = commands.Bot(command_prefix="+")
+client.remove_command('help')
+
 with open('EmojiToCountry.json', encoding='utf8') as json_file:
     data = json.load(json_file)
 
@@ -23,14 +35,57 @@ SpecialCharacterPattern = re.compile(r"^[_A-z0-9]*((-|\s)*[_A-z0-9])*$")
 @client.event
 async def on_ready():
     print('Bot is ready.')
+    LargeIMGUrl = "https://novacrypt.org/assets/images/novacrypt.png"
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="NOVACRYPT"))
 
 @client.event
 async def on_member_join(member):
     print(f'{member.display_name} just joined')
     for channel in member.guild.channels:
-        if channel.name == "general":
-            await channel.send(f'Please type `+nickname (Username)` to change your username')
+        if channel.name == DEFAULT_NICKNAME_CHANNEL:
+            await channel.send(f'{member.display_name} please type `+nickname (Username)` to change your username')
 
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+    embed.set_author(name="Help Menu")
+    embed.add_field(name=f'{ctx.prefix}help',value="Sends this embed to you.",inline=False)
+    embed.add_field(name=f'{ctx.prefix}nickname',value="Use this in a server to change your nickname!",inline=False)
+    embed.add_field(name=f'{ctx.prefix}convert',value=f"Converts ISO codes to locations. Try {ctx.prefix}convert for more info.",inline=False)
+
+    await author.send(embed=embed)
+
+#Converts ISO3 Tags to Country Names
+@client.command(aliases=["country","ISO","Country"])
+async def convert(ctx,Country,*State):
+    Country = Country.upper()
+    with open('CountryStateCodes.json',encoding='utf8') as ConversionTableJSON:
+        ConversionTable = json.load(ConversionTableJSON)
+        ConversionTableJSON.close()
+        if Country in ConversionTable and len(Country)==2 and SpecialCharacterPattern.match(Country):
+            response = "The country name is " + ConversionTable[Country]["name"] + ". "
+            try:
+                StateClean = SpecialCharacterPattern.match(State[0])
+                State = Country + "-" + State[0].upper()
+                if State in ConversionTable[Country]["divisions"] and len(State)<=6 and StateClean:
+                    response = response + "The state/province/city name is " + ConversionTable[Country]["divisions"][State] + "."
+                else:
+                    response = response + "The state/province/city was unable to be found."    
+            except:
+                pass
+            await ctx.send(response)
+        else:
+            await ctx.send("The country code was invalid, try again.")
+@convert.error
+async def convert_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Use this command to convert the tags from nicknames in the structure of `Nickname | [Region_Code] | [Country_Code]` to real locations.\nType `{ctx.prefix}convert (Country_Code) (Optional: Region_Code)`')
+
+
+#Allows the user to change their nickname.
 @client.command(aliases=["nick","Nick","Nickname"])
 async def nickname(ctx, *, nickname):
     if len(nickname) <= 20 and SpecialCharacterPattern.match(nickname):
@@ -78,6 +133,10 @@ async def nickname(ctx, *, nickname):
                 Complete_Nickname = f"{nickname} | {StateSelect.content} | {USER_COUNTRY_ID}"
                 await ctx.message.author.edit(nick=Complete_Nickname)
                 await ctx.send(f"Sucess! Your nickname has successfully been changed to {Complete_Nickname}.")
+                for role in ctx.guild.roles:
+                    if role.id == BASE_ROLE:
+                        ADD_ROLE = role
+                await ctx.message.author.add_roles(ADD_ROLE,reason="Nickname Sucessfully Created")
             else:
                 await ctx.send(f"Command was unsuccessful, make sure the Code matches the ones offered exactly.")
             for embed_item in EMBEDS:
@@ -98,11 +157,26 @@ async def nickname_error(ctx, error):
         await ctx.send(f'Use this command to change your nickname, type `{ctx.prefix}nickname (Username)`')
 
 
-@tasks.loop(seconds=20.0)
+@tasks.loop(minutes=60.0)
 async def NicknameCollection():
-    async for guild in client.fetch_guilds():
+    for ID in GUILD_IDS:
+        guild = client.get_guild(ID)
+        guild_name_filtered = guild.name.split(".")[0]
+        src = os.path.join(os.getcwd(),"BaseGuildJson.json")
+        dst = os.path.join(os.getcwd(),f"GuildFiles",f"{guild_name_filtered}.json")
+        shutil.copyfile(src,dst)
+        GuildMemberFile = open(f"GuildFiles/{guild_name_filtered}.json","r")
+        GuildMemberJSON = json.load(GuildMemberFile)
+        GuildMemberFile.close()
         for member in guild.members:
-            print(member.display_name)
+            try:
+                GuildMemberJSON[member.display_name[-2:]] = GuildMemberJSON[member.display_name[-2:]] + 1
+            except KeyError:
+                pass
+                #print(f"Key not found for {member.display_name}. In {guild.name}")
+        GuildMemberFile = open(f"GuildFiles/{guild_name_filtered}.json","w")
+        json.dump(GuildMemberJSON,GuildMemberFile,indent=4)
+        GuildMemberFile.close()
 
 @NicknameCollection.before_loop
 async def wait_for_NicknameCollection():
